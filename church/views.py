dcb183c26299dd96c7b220773964fecab19a1ace
horlamiedea <horlamiedea@gmail.com>
2021-03-25 15:01:39 +0100

    pagination working in views.py

--- church/views.py
+++ church/views.py
@@ -1,9 +1,11 @@
+from django.core import paginator
 from django.db import models
 from django.db.models.base import Model
 from django.shortcuts import render
 from django.contrib import messages
 from django.views.generic import View
 from .models import Contact, Events
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -12,10 +14,13 @@ class Home(View):
 
     def get(self, *args, **kwargs):
         events = Events.objects.all()
-        context = {
-            'events': events
-        }
-        return render(self.request, 'index.html', context)
+        paginator = Paginator(events, 5)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        # context = {
+        #     'events': events
+        # }
+        return render(self.request, 'index.html', {'page_obj': page_obj})
 
     def post(self, *args, **kwargs):
         form = Contact(self.request.POST)
